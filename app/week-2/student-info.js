37fc6daee270d7d6ba0414cba170e06ef3901b58
NM715 <Nawal.Mohamuud@edu.sait.ca>
2025-01-28 14:56:45 -0700

    latests

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div> 
+      <p>Nawal M</p>
+      <Link href="https://github.com/NM715">Github Profile</Link>
+    </div>
+  );
+}
+
